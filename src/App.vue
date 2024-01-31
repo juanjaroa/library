d1fccc613e9560ef0d6c70fc0581fb5d547654a3
jroa <juanjaroa@gmail.com>
2024-01-30 20:11:12 -0500

    Layout enhanced and SpeedDial added as nav-menu

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,64 @@
 <script setup>
+import { useRouter, RouterView } from "vue-router";
+import { useAuthStore } from "@/stores/AuthStore";
+import { ref } from "vue";
+import SpeedDial from "primevue/speeddial";
 import BooksTable from "./components/BooksTable.vue";
 import BookForm from "./components/BookForm.vue";
 import LoginForm from "./components/LoginForm.vue";
+
+const router = useRouter();
+const authStore = useAuthStore();
+
+const items = ref([
+  {
+    label: "Home",
+    icon: "pi pi-home",
+    command: () => {
+      router.push("/");
+    },
+  },
+  {
+    label: "Login",
+    icon: "pi pi-key",
+    command: () => {
+      router.push("/login");
+    },
+  },
+  {
+    label: "Agregar Libro",
+    icon: "pi pi-book",
+    command: () => {
+      router.push("/add-book");
+    },
+  },
+  /* {
+    label: "Vue Website",
+    icon: "pi pi-external-link",
+    command: () => {
+      window.location.href = "https://vuejs.org/";
+    },
+  }, */
+]);
 </script>
 
 <template>
-  <LoginForm />
+  <RouterView />
+
+  <BookForm v-if="authStore.localSession?.data.session" />
   <BooksTable />
-  <BookForm />
+  <SpeedDial
+    :model="items"
+    :tooltipOptions="{ position: 'left' }"
+    buttonClass="p-button-outlined"
+    showIcon="pi pi-bars"
+    hideIcon="pi pi-times"
+    :radius="60"
+    type="quarter-circle"
+    direction="up-left"
+    :style="{ right: '1rem', bottom: '1rem' }"
+    :transitionDelay="125"
+  />
 </template>
 
-<style scoped>
-section {
-  display: flex;
-  flex-direction: column;
-  gap: 0.75rem;
-  align-items: flex-start;
-  max-width: 600px;
-  width: 50%;
-  min-width: 300px;
-}
-</style>
+<style scoped></style>
