933209b4e50d2c831f05804f0db9f7b6297071f1
jroa <juanjaroa@gmail.com>
2024-01-30 20:07:35 -0500

    Added SingleBook,vue neasted in Bookstable.vue

---
+++ src/components/SingleBook.vue
@@ -0,0 +1,28 @@
+<!-- DetallesLibro.vue -->
+<script setup>
+import { ref, computed } from "vue";
+import { useBookStore } from "@/stores/BookStore";
+
+const booksStore = useBookStore();
+const props = defineProps(["showBook"]);
+
+const currentBook = computed(() => {
+  return booksStore.books.find((book) => book.id === props.showBook.id);
+});
+
+const emit = defineEmits(["null-book"]);
+
+function closeBook() {
+  emit("null-book");
+}
+</script>
+
+<template>
+  <div v-if="currentBook">
+    <h2>{{ currentBook.title }}</h2>
+    <p>Autor: {{ currentBook.author }}</p>
+    <!-- Otros detalles del libro -->
+  </div>
+  <p v-else>Libro no encontrado</p>
+  <button @click="closeBook">close</button>
+</template>
