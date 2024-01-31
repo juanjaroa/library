143362353d57c997582a2d67d30a3df062e03e40
jroa <juanjaroa@gmail.com>
2024-01-30 20:37:30 -0500

    AuthStore logic added

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>New book page</div>
-  <BookForm />
+  <BookForm v-if="authStore.localSession?.data.session" />
+  <h2 v-else>Ups... No estas autorizad@!</h2>
 </template>
 <script setup>
+import { useAuthStore } from "@/stores/AuthStore";
+const authStore = useAuthStore();
+
 import BookForm from "@/components/BookForm.vue";
 </script>
 <style scoped></style>
